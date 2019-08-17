9e131609a9845b663d051b283779b0ee8a85196b
LazyDuck <lazyduckdev@gmail.com>
2019-08-17 19:38:14 +0200

    Working on selecting/deselectig ar objects

--- ProjectMobidev/View/MainViewController.swift
+++ ProjectMobidev/View/MainViewController.swift
@@ -17,11 +17,13 @@ class MainViewController: UIViewController, ARSCNViewDelegate {
     
     var editMode = false
     
-    var initialPanLocation : SCNVector3?
+    var initialPanDepth : CGFloat?
     
     var lastPanLocation : SCNVector3?
     
-    var draggingNode : SCNNode?
+    var selectedObject : SCNNode?
+    
+    var graphs : [SCNNode?] = []
     
     override var prefersStatusBarHidden: Bool
     {
@@ -80,30 +82,41 @@ class MainViewController: UIViewController, ARSCNViewDelegate {
         
         if let imageAnchor = anchor as? ARImageAnchor
         {
-            let plane = SCNPlane(width: imageAnchor.referenceImage.physicalSize.width, height: imageAnchor.referenceImage.physicalSize.height)
-            
-            plane.firstMaterial?.diffuse.contents = UIColor(white: 1, alpha: 0.8)
-            
-            let planeNode = SCNNode(geometry: plane)
-            
-            planeNode.eulerAngles.x = -.pi/2
-            
-            node.addChildNode(planeNode)
-            
-            if let shipScene = SCNScene(named: "art.scnassets/ship.scn")
-            {
-                let shipNode = shipScene.rootNode.childNodes.first
-                shipNode?.position = SCNVector3Zero
-                shipNode?.position.z = 0.15
-                planeNode.addChildNode(shipNode!)
-            }
-            
+            displayWhitePlane(imageAnchor: imageAnchor, node: node)
         }
         
         return node
     }
     
-    //Input Events
+    //View input
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        if let location = touches.first?.location(in: sceneView)
+        {
+            let result = getTouchedObject(location: location)
+            
+            if isInteractiveObject(object: result)
+            {
+                selectedObject = result?.node
+                highlightSelectedObject()
+            }
+            
+            selectedObject = nil
+            editMode=false
+        }
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        if selectedObject != nil
+        {
+            hideSelectedObject()
+            selectedObject = nil
+        }
+    }
+    
+    //Input buttons
     
     @IBAction func btnAddTouchDown(_ sender: Any) {
         
@@ -114,59 +127,6 @@ class MainViewController: UIViewController, ARSCNViewDelegate {
         else
         {
             self.performSegue(withIdentifier: "toDownloadFileListViewController", sender: nil)
-        }
-    }
-    
-    @IBAction func tapEvent(_ gestureRecognizer: UITapGestureRecognizer)
-    {
-        guard gestureRecognizer.view != nil else { return }
-        print("Edit mode off")
-        editMode = false
-    }
-    
-    @IBAction func panEvent(_ gestureRecognizer: UIPanGestureRecognizer)
-    {
-        guard gestureRecognizer.view != nil && editMode else { return }
-        let location = gestureRecognizer.location(in: sceneView)
-        
-        switch gestureRecognizer.state {
-        case .began:
-            print("pan begin")
-            guard let hitNodeResult = sceneView.hitTest(location, options: nil).first else { return }
-            initialPanLocation = sceneView.projectPoint(lastPanLocation!)
-            lastPanLocation = hitNodeResult.worldCoordinates
-            draggingNode = hitNodeResult.node
-            
-        case .changed:
-            let z = CGFloat(initialPanLocation!.z)
-            
-            let deltaVector = SCNVector3(x: Float(location.x), y: Float(location.y), z: Float(z))
-            
-            let worldTouchPosition = sceneView.unprojectPoint(deltaVector)
-            
-            let movementVector = SCNVector3(
-                worldTouchPosition.x - lastPanLocation!.x,
-                worldTouchPosition.y - lastPanLocation!.y,
-                worldTouchPosition.z - lastPanLocation!.z)
-            draggingNode?.localTranslate(by: movementVector)
-            self.lastPanLocation = worldTouchPosition
-        default:
-            print("pan end")
-        }
-    }
-    
-    @IBAction func longPressEvent(_ gestureRecognizer: UILongPressGestureRecognizer)
-    {
-        if gestureRecognizer.state == .began
-        {
-            let touchLocation = gestureRecognizer.location(in: sceneView)
-            let hitTest = sceneView?.hitTest(touchLocation, options: nil)
-            
-            if !hitTest!.isEmpty
-            {
-                print("Edit mode on")
-                editMode = true
-            }
         }
     }
     
@@ -183,8 +143,176 @@ class MainViewController: UIViewController, ARSCNViewDelegate {
             print("ARKit Scene Successfully Saved")
         }
     }
+
+    //Gesture recognizer actions
+    
+    @IBAction func tapEvent(_ gestureRecognizer: UITapGestureRecognizer)
+    {
+        guard gestureRecognizer.view != nil else { return }
+    }
+    
+    @IBAction func panEvent(_ gestureRecognizer: UIPanGestureRecognizer)
+    {
+        guard gestureRecognizer.view != nil && editMode else { return }
+        /*let location = gestureRecognizer.location(in: sceneView)
+        
+        switch gestureRecognizer.state {
+        case .began:
+            //Controllo se c'è qualcosa da muovere
+            guard let hitTestResult = sceneView.hitTest(location, options: nil).first else { return }
+            lastPanLocation = hitTestResult.worldCoordinates
+            initialPanDepth = CGFloat(sceneView.projectPoint(lastPanLocation!).z)
+            selectedObject = hitTestResult.node
+            print(lastPanLocation)
+        case .changed:
+            let worldTouchPosition = sceneView.unprojectPoint(SCNVector3(location.x, location.y, initialPanDepth!))
+            let movementVector = SCNVector3(
+                worldTouchPosition.x - lastPanLocation!.x,
+                worldTouchPosition.y - lastPanLocation!.y,
+                worldTouchPosition.z - lastPanLocation!.z)
+            selectedObject?.localTranslate(by: movementVector)
+            self.lastPanLocation = worldTouchPosition
+            print(lastPanLocation)
+            break
+        default:
+            print("stop pan")
+            break
+        }*/
+    }
+    
+    @IBAction func longPressEvent(_ gestureRecognizer: UILongPressGestureRecognizer)
+    {
+        /*switch gestureRecognizer.state {
+        case .began:
+            print("began")
+            break
+        case .possible:
+            print("possible")
+            break
+        case .cancelled:
+            print("cancelled")
+            break
+        case .ended:
+            print("ended")
+            break
+        case .changed:
+            print("changed")
+            break
+        case .failed:
+            print("failed")
+            break
+        default:
+            print("default")
+            break
+        }*/
+        /*if gestureRecognizer.state == .began
+        {
+            let touchLocation = gestureRecognizer.location(in: sceneView)
+            let hitTest = sceneView?.hitTest(touchLocation, options: nil)
+            
+            if !hitTest!.isEmpty
+            {
+                print("Edit mode on")
+                editMode = true
+            }
+        }*/
+    }
     
     //Utility functions
+    
+    func highlightSelectedObject()
+    {
+        guard selectedObject != nil else { return }
+        
+        let oldColor = UIColor.red
+        let newColor = UIColor(red: 207, green: 0, blue: 1, alpha: 1)
+        
+        let duration: TimeInterval = 0.1
+        let act0 = SCNAction.customAction(duration: duration, action: { (node, elapsedTime) in
+            let percentage = elapsedTime / CGFloat(duration)
+            node.geometry?.firstMaterial?.diffuse.contents = self.changeColor(from: newColor, to: oldColor, percentage: percentage)
+        })
+        
+        let act = SCNAction.sequence([act0])
+        selectedObject?.runAction(act)
+    }
+    
+    func hideSelectedObject()
+    {
+        
+    }
+    
+    func changeColor(from: UIColor, to: UIColor, percentage: CGFloat) -> UIColor {
+        let fromComponents = from.cgColor.components!
+        let toComponents = to.cgColor.components!
+        
+        let color = UIColor(red: fromComponents[0] + (toComponents[0] - fromComponents[0]) * percentage,
+                            green: fromComponents[1] + (toComponents[1] - fromComponents[1]) * percentage,
+                            blue: fromComponents[2] + (toComponents[2] - fromComponents[2]) * percentage,
+                            alpha: fromComponents[3] + (toComponents[3] - fromComponents[3]) * percentage)
+        return color
+    }
+    
+    func displayWhitePlane(imageAnchor: ARImageAnchor, node: SCNNode)
+    {
+        DispatchQueue.main.async
+        { [weak self] in
+            let plane = SCNPlane(width: imageAnchor.referenceImage.physicalSize.width, height: imageAnchor.referenceImage.physicalSize.height)
+            
+            plane.firstMaterial?.diffuse.contents = UIColor(white: 1, alpha: 0.8)
+            
+            let planeNode = SCNNode(geometry: plane)
+            
+            planeNode.eulerAngles.x = -.pi/2
+            
+            node.addChildNode(planeNode)
+            
+            let (min, max) = planeNode.boundingBox
+            
+            let size = SCNVector3Make(max.x - min.x, max.y - min.y, max.z - min.z)
+            
+            let widthRatio = Float(imageAnchor.referenceImage.physicalSize.width)/size.x
+            let heightRatio = Float(imageAnchor.referenceImage.physicalSize.height)/size.z
+            // Pick smallest value to be sure that object fits into the image.
+            let finalRatio = [widthRatio, heightRatio].min()!
+            
+            let appearAnimation = SCNAction.scale(to: CGFloat(finalRatio), duration: 0.4)
+            
+            appearAnimation.timingMode = .easeOut
+            
+            planeNode.scale = SCNVector3(0.001, 0.001, 0.001)
+            
+            planeNode.runAction(appearAnimation)
+            if let shipScene = SCNScene(named: "art.scnassets/ship.scn")
+            {
+                 let shipNode = shipScene.rootNode.childNodes.first
+                 shipNode?.position = SCNVector3Zero
+                 shipNode?.position.z = 0.15
+                 self?.graphs.append(shipNode)
+                 planeNode.addChildNode(shipNode!)
+            }
+        }
+    }
+    
+    func isInteractiveObject(object: SCNHitTestResult?) -> Bool
+    {
+        guard object != nil else { return false }
+        guard let node = object?.node else { return false }
+        
+        if graphs.contains(node)
+        {
+            print("yikes")
+        }
+        
+        return true
+    }
+    
+    func getTouchedObject(location: CGPoint) -> SCNHitTestResult?
+    {
+        //let options = SCNHitTestOption(rawValue: SCNHitTestOption.firstFoundOnly.rawValue)
+        //return sceneView.hitTest(po)
+        return sceneView.hitTest(location, options: [SCNHitTestOption.firstFoundOnly:true]).first
+    }
     
     func authorizeApp(){
         DropboxClientsManager.authorizeFromController(UIApplication.shared,
